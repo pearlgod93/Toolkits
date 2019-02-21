98a869ef23e3f038a20e50d0bd92ce41f9455871
pearlgod93 <47864995+pearlgod93@users.noreply.github.com>
2019-02-21 14:57:48 -0500

    Create audiotovector.py

---
+++ audiotovector.py
@@ -0,0 +1,10 @@
+#Libraries-to-import
+
+import soundfile as sf
+import pyplotlib as plot
+
+#Data-Extraction
+d, samplerate = sf.read('<file_name>.wav') #If it is present in the existing directory
+
+#Visualize-the-wave-file
+def conmono:
